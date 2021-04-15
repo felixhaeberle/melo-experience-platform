cced85e910f6bb765c40da476353b9c5b1f67be0
Tobias Michaely <t_michaely@web.de>
2021-04-15 15:46:01 +0200

    update api data structure

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,10 @@
 
 export default (req, res) => {
   let hostURL = req.headers.host;
-  res.status(200).json( {
-    clickAction: {behaviour: 'click', sound: hostURL + '/examples/scroll/click.mp3', settings: {delay: 500}},
-  })
+  res.status(200).json( [
+      {name: 'clickAction', behaviour: 'click', sound: hostURL + '/examples/scroll/click.mp3', settings: {delay: 500}},
+      {name: 'hoverInAction', behaviour: 'hoverIn', sound: hostURL + '/examples/scroll/hoverIn.ogg', settings: {}},
+      {name: 'hoverOutAction', behaviour: 'hoverOut', sound: hostURL + '/examples/scroll/hoverIn.ogg', settings: {}},
+  ])
 }
+
