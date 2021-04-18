27e043750b1b1ed4964120f4e8b28cffd02682c7
Niklas.Buchfink <niklas.buchfink@web.de>
2021-04-18 03:17:10 +0200

    Drag Example Update

--- components/examples/drag/drag.js
+++ components/examples/drag/drag.js
@@ -1,68 +1,56 @@
 import styles from '../../../styles/drag.module.css'
 
 export default function Drag() {
-    // Audio
-    const dragSound = new Audio('/examples/drag/ui_drag.wav');
-    const dropSound = new Audio('/examples/drag/ui_drop.wav');
+  // Audio
+  const dragSound = new Audio('/examples/drag/ui_drag.wav');
+  const dropSound = new Audio('/examples/drag/ui_drop.wav');
 
-    const loopOn = () => {
-      dragSound.loop = true;
-    }
+  const loopOn = () => {
+    dragSound.loop = true;
+  }
 
-    const loopOff = () => {
-      dragSound.loop = false;
-    }
+  const loopOff = () => {
+    dragSound.loop = false;
+  }
+  
+  window.onload = addListeners;
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-    // Make the DIV element draggable:
-    dragElement(document.getElementById("dragSample"));
+  function addListeners(){
+      document.getElementById('dragSample').addEventListener('mousedown', mouseDown, false);
+      window.addEventListener('mouseup', mouseUp, false);
+  }
 
-    function dragElement(elmnt) {
-      var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-      if (document.getElementById(elmnt.id + "header")) {
-        // if present, the header is where you move the DIV from:
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-      } else {
-        // otherwise, move the DIV from anywhere inside the DIV:
-        elmnt.onmousedown = dragMouseDown;
-      }
+  function mouseUp()
+  {
+    dropSound.play();
+    dragSound.pause();
+    dragSound.load();
+    window.removeEventListener('mousemove', divMove, true);
+  }
 
-      function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+  function mouseDown(e){
+    dragSound.play();
+    dropSound.pause();
+    dropSound.load();
+    window.addEventListener('mousemove', divMove, true);
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+  }
 
-        dragSound.play();
-        dropSound.pause();
-        dropSound.load();
-      }
+  function divMove(e){
+    var div = document.getElementById('dragSample');
 
-      function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
-
-      function closeDragElement() {
-        // stop moving when mouse button is released:
-        dropSound.play();
-        dragSound.pause();
-        dragSound.load();
-        document.onmouseup = null;
-        document.onmousemove = null;
-      }
-    }
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    div.style.top = (div.offsetTop - pos2) + "px";
+    div.style.left = (div.offsetLeft - pos1) + "px";
+  }
 
   return (
     <div className={styles.wrapper}>
